e266e3892bf4aa7b605c283c1d2a63fe79212185
robonexx <robertwagar@gmail.com>
2023-03-10 11:17:10 +0100

    update about and nav on mobile, padding nav_items

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -3,24 +3,30 @@ import Image from 'next/image'
 import {motion} from 'framer-motion'
 import IMGFANCY from '/public/images/tony_fancy.png'
 import IMGJUDGE from '/public/images/tony_judge.png'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 
 // styles
 import styles from './About.module.scss'
 
 const About = () => {
+
+  const desktop = useMediaQuery(768)
+
   return (
       <div className={styles.about}>
       <motion.div className={styles.img_wrapper} 
         initial={{scale: 0}}
-        animate={{scale: 1}}
+        animate={{
+          scale: 1,
+          width: desktop ? '90vw' : '60%',
+        height: desktop ? '40vh' : '55vh'}}
         transition={{duration: 0.6, delay: 0.8}}
       >
         <Image
-          className={styles.img}
           src={IMGJUDGE}
           alt="tony judge solo"
-          width='60%'
-          height='auto'
+          fill
+          priority
         />
       </motion.div>
       <motion.h2
@@ -42,14 +48,19 @@ const About = () => {
               While dancing with his  partners (James &apos; OG Skeeter Rabbit&apos; Higgins, Edwin &apos;Buddy GoGo&apos; Lombard, and Kevin YoYo ) they formed another group by the name of Creative Generation - formed by The Watts Writers Workshop to develop talent around in Watts, Compton adn LA! Creative generation was the first unisex locking group. <br />
               Partners in that group were Arnetta Johnson, Lorna Dune, Greg Cambellock Jr, Jimmy Scooby-Doo Foster and Tony&apos;s fellow GoGo brothers ... But most of you remember Tony GoGo as a member of the Original Lockers with Don Campbell, Fluky Luke, Mr. Penguine, Shabba-Doo, and Slim The Robot.
       </motion.p>
-      <div className={styles.img_wrapper}>
-      <Image src={IMGFANCY} alt="tony in costume"
-          className={styles.img}
-          width='60%'
-          height='auto'
+      <motion.div className={styles.img_wrapper}
+        animate={{
+          scale: 1,
+          width: desktop ? '60vw' : '30%',
+          height: desktop ? '50vh' : '60vh'
+        }}>
+        <Image src={IMGFANCY}
+          alt="tony in costume"
+         fill
+          priority
             />
 
-      </div>
+      </motion.div>
  <p>
 Since leaving the Original Lockers group in the early 1980s, Tony has taught and developed not only the original art of Locking in Japan but he recognised, created and developed a style that has by now become unique to the Asian style of Locking, sometimes refered to as &apos;Lock Fusion&apos;. Tony Go Go is now considered the innovator of the Japanese style of Locking.
 <br/>
--- src/pages/media/index.jsx
+++ src/pages/media/index.jsx
@@ -1,11 +1,10 @@
-/* import ImageGallery from '@/components/imagegallery/ImageGallery' */
 import React from 'react'
 
 const index = () => {
   return (
       <div>
           <h1>Media</h1>
-          {/* <ImageGallery /> */}
+         
     </div>
   )
 }
